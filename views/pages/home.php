1e65cea83c820b70dc87e184879a20ffd1cf1927
Joao Pedro Romao <ojpzinho@gmail.com>
2023-11-14 14:56:49 -0300

    fixed some paths

--- views/pages/home.php
+++ views/pages/home.php
@@ -6,7 +6,7 @@
       <h2 class="align-left margin-top-20 padding-left-right-20">Selecione os ingredientes:</h2>
 
       <div class="cards-container" id="cards">
-        <img src="./img/loading.gif" alt="loading spinner" width="100px" />
+        <img src="<?php echo FULL_PATH . '/views/assets/img/loading.gif'; ?>" alt="loading spinner" width="100px" />
       </div>
 
       <button class="btn align-self-right disabled" id="nextBtn">Próximo</button>
@@ -14,4 +14,4 @@
   </main>
 </div>
 
-<script src="<?php echo FULL_PATH . '/views/js/home.js' ?>" type="module"></script>
+<script src="<?php echo FULL_PATH . '/views/js/home.js'; ?>" type="module"></script>
--- views/templates/head.php
+++ views/templates/head.php
@@ -4,9 +4,9 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-  <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
+  <link rel="shortcut icon" href="<?php echo FULL_PATH . '/views/assets/img/favicon.ico'; ?>" type="image/x-icon">
 
-  <link rel="stylesheet" href="<?php echo FULL_PATH . '/views/styles/global.css' ?>" />
+  <link rel="stylesheet" href="<?php echo FULL_PATH . '/views/styles/global.css'; ?>" />
 
   <?php
   if (isset($data["styles"]) && !empty($data["styles"])) {
@@ -19,21 +19,21 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <meta name="description" content="<?php echo $data['description'] ?>" />
+  <meta name="description" content="<?php echo $data['description']; ?>" />
   <meta name="keywords" content="PedePizza, Pizzaria, Pizza, Delivery, Pizzaria Online" />
   <meta name="author" content="PedePizza" />
 
   <meta property="og:type" content="website" />
   <meta property="og:title" content="PedePizza" />
-  <meta property="og:description" content="<?php echo $data['description'] ?>" />
-  <meta property="og:image" content="./img/logo.svg" />
-  <meta property="og:url" content="https://pedepizza.com.br" />
+  <meta property="og:description" content="<?php echo $data['description']; ?>" />
+  <meta property="og:image" content="<?php echo FULL_PATH . '/views/assets/img/logo.svg'; ?>" />
+  <meta property="og:url" content="<?php echo FULL_PATH; ?>" />
   <meta property="og:site_name" content="PedePizza" />
 
   <meta name="twitter:title" content="PedePizza" />
-  <meta name="twitter:description" content="<?php echo $data['description'] ?>" />
-  <meta name="twitter:image" content="./img/logo.svg" />
+  <meta name="twitter:description" content="<?php echo $data['description']; ?>" />
+  <meta name="twitter:image" content="<?php echo '/views/assets/img/logo.svg'; ?>" />
   <meta name="twitter:card" content="summary_large_image" />
 
-  <title><?php echo $data["title"] ?></title>
+  <title><?php echo $data["title"]; ?></title>
 </head>
